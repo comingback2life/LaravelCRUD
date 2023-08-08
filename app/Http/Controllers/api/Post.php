53ed582c4247c7367fb3a5ce3331bf180ca33088
Samip Pokharel <callmesamip@gmail.com>
2023-08-08 14:59:04 +1000

    Initial Commit

---
+++ app/Http/Controllers/api/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class Post extends Controller
+{
+    public function getAllPosts(){
+        $posts = Post::all();
+        if($posts->count()>0){
+            return response()->json([
+                'status'=>200,
+                'message'=>'Posts found'
+            ])
+        }
+        return response()->json([
+            'status'=>404,
+            'message'=>'No post found!'
+        ],404);
+    }
+}
